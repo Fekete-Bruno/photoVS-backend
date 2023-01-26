37bcfdb1522d93913a727547342348b51310e7d4
Fekete <brunohffekete@gmail.com>
2023-01-26 17:12:43 -0300

    chore: moved health.test to folder

--- backend/tests/integration/health.test.ts
+++ backend/tests/integration/health.test.ts
@@ -1,11 +1,11 @@
 import httpStatus from "http-status";
 import supertest from "supertest";
-import app from "../src/app";
+import app from "../../src/app";
 
 const server = supertest(app);
 
 describe("GET /health", ()=>{
-    it("should respond with status 200 with correct text ", async() =>{
+    it("should respond with status 200 and correct text ", async() =>{
         const response = await server.get("/health");
 
         expect(response.status).toBe(httpStatus.OK);
